6f0310ab6cc91847d226791de0f6fc879f63e2ec
Ng Yvonne <88yvonne.ng@gmail.com>
2024-08-14 11:32:29 +0800

    remove global variable, change manager.json to manager.txt

--- manager.py
+++ manager.py
@@ -1,33 +1,41 @@
 import re  # import the regular expressions(regex), a type of text pattern matching tool to check if a string contains the specified search pattern
 import json  # import json text file to record data
 
-# Create a dictionary named 'info'
-info = {}
-
 
 # Define the function that loads data from the file
 def load_data_from_file():
-    global info  # to access 'info' dictionary outside the function
     try:
-        file = open('manager.json', 'r')  # open the file and read
-        content = file.read().strip()  # Read the content and strip any whitespace
-        if content:  # Check if the file is not empty
-            info = json.loads(content)
+        file = open('manager.txt', 'r')  # open the file and read
+        content = file.read().strip()  # strip() function is used to strip any unnecessary whitespaces
+        file.close()  # close the file after reading
+        if content:  # start to check if the file is not empty
+            try:
+                return json.loads(content)  # parse the content as json format into python dictionary and return the content if successfully parsed
+            except json.JSONDecodeError:
+                return {}  # return empty dictionary if the content does not parse successfully
         else:
-            info = {}  # Initialize as an empty dictionary if the file is empty
+            return {}  # return empty dictionary if the file is empty
     except FileNotFoundError:
-        info = {}  # Initialize as an empty dictionary if the file does not exist
+        return {}  # return empty dictionary if the file does not exist
 
 
 # Define the function that saves information to the file
-def save_info():
-    file = open('manager.json', 'w')  # open the file to write
+def save_info(info):
+
+    file = open('manager.txt', 'w')  # open the file to write
     json.dump(info, file,indent=4)  # convert the dictionary into JSON format, 4 spaces indentation make it clearer for visualization
+    file.close()  # close the file after writing
+
+
+print('---------------------------------')
+print('MANAGER PAGE')
+print('---------------------------------')
 
 
 # create a function for manager
 def manager():
-    load_data_from_file()
+
+    info = load_data_from_file()  # initialize 'info' as the name of dictionary that store data loaded from file
 
     manager_username = input('Please enter your username: ')
     manager_password = input('Please enter your password: ')
@@ -45,8 +53,26 @@ def manager():
                 'e. customer feedback'
             )
             choice = input('Select a choice (a, b, c, d, e): ')
+            if choice in ['a', 'b', 'c', 'd', 'e']:
+                if choice == 'a':
+                    print('enter again.')
+
+                elif choice == 'b':
+                    print('enter again.')
+
+                elif choice == 'c':
+                    print('enter again.')
+
+                elif choice == 'd':
+                    print('enter again.')
+
+                elif choice == 'e':
+                    print('enter again.')
+
+            else:
+                print('enter again.')
         else:
-            print('Username or password invalid, please enter again or try another account.')  # prompt user to input again if it does't match
+            print('Username or password invalid, please enter again or try another account.')  # prompt user to input again if it does't match with data in file
             manager()
     else:
         if len(manager_password) < 8 or len(manager_password) > 12:  # to ensure the password entered is between 8 to 12 digits
@@ -70,16 +96,14 @@ def manager():
                     print('Please enter a valid age.')
 
             while True:
-                gender = input('Gender(m=male, f=female, x=prefer not to say): ')
-                if gender not in ['f', 'm', 'x']:
+                gender = input('Gender(m=male, f=female): ')
+                if gender not in ['f', 'm']:
                     print('Invalid input. Please enter again.')
                 else:
                     if gender == 'f':
                         gender = 'female'
-                    elif gender == 'm':
-                        gender = 'male'
                     else:
-                        gender = 'prefer not to say'
+                        gender = 'male'
                     break  # Exit the loop if age is valid
 
             while True:
@@ -111,12 +135,12 @@ def manager():
             }
 
             # write the updates into file
-            save_info()
+            save_info(info)
             print('Information saved.\n')  # let user know their information are saved
             print('--------------------------------------------------')
             print('Thank you for completing the personal information.')
             print('--------------------------------------------------')
-            print('For security purpose, please log in again to access the system.')
+            print('For security purpose, please log in again to access the system.\n')
             manager()  # prompt user to login again after filled out their information
 
 
