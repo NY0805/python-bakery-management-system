ff70b5c57d09714750df6028acef8a2e14bd07b0
Ng Yvonne <88yvonne.ng@gmail.com>
2024-08-24 14:48:33 +0800

    fixed some bugs

--- system_administration.py
+++ system_administration.py
@@ -12,22 +12,26 @@ def system_administration():
         print('1. Baker\n2. Cashier\n3. Customer\n4. Exit')
 
         role = input('Which role do you want to manage(1, 2, 3, 4): ')
-        if role not in [1, 2, 3, 4]:
-            print('Invalid input.')
 
-        while role == '1':
+        if (role != '1') and (role != '2') and (role != '3') and (role != '4'):
+            print('invalid input.')
+
+        elif role == '1':
             system_administration_baker.system_administration_baker()
 
-        while role == '2':
+        elif role == '2':
             system_administration_cashier.system_administration_cashier()
 
-        while role == '3':
+        elif role == '3':
             system_administration_customer.system_administration_customer()
 
-        while role == '4':
+        elif role == '4':
             print('Exiting to manager privilege......')
             break
         break
+
+
+
 
 
 
