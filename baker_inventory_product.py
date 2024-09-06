b4448e3acb397f235ad18b9909bf44fb51b72283
hxlum <xunhanlum0803@gmail.com>
2024-09-06 14:41:16 +0800

    product management

--- baker_inventory_product.py
+++ baker_inventory_product.py
@@ -42,23 +42,20 @@ def product_management():
     print('4. Back to Previous Page')
 
     while True:
-        try:
-            option_product_management = input('\nPlease choose a service:'
-                                              '\n>>> ')
-            if option_product_management not in ['1', '2', '3', '4']:
-                print('Please enter a valid number.')
-            else:
-                if option_product_management == '1':
-                    product_categories()
-                elif option_product_management == '2':
-                    pass
-                elif option_product_management == '3':
-                    pass
-                elif option_product_management == '4':
-                    print('Exiting to previous page...')
-                    break
-        except ValueError:
+        option_product_management = input('\nPlease choose a service:'
+                                          '\n>>> ')
+        if option_product_management not in ['1', '2', '3', '4']:
             print('Please enter a valid number.')
+        else:
+            if option_product_management == '1':
+                product_categories()
+            elif option_product_management == '2':
+                pass
+            elif option_product_management == '3':
+                pass
+            elif option_product_management == '4':
+                print('Exiting to previous page...')
+                break
 
 
 def product_categories():
@@ -70,23 +67,48 @@ def product_categories():
     print('5. Muffins')
     print('6. Others')
     print('7. Back to Previous Page')
+
+    category = None
+
     while True:
-        try:
-            option_product_categories = input('\nPlease input the category:'
-                                              '\n>>> ')
-            if option_product_categories not in ['1', '2', '3', '4', '5', '6', '7']:
-                print('Please enter a valid number.')
-                continue
-            else:
-                if option_product_categories == '7':
-                    print('Going back to the previous page.')
-                    product_management()
-                    return option_product_categories
-                product_details()
-                return option_product_categories
-        except ValueError:
+        option_product_categories = input('\nPlease input the category:'
+                                          '\n>>> ')
+        if option_product_categories not in ['1', '2', '3', '4', '5', '6', '7']:
             print('Please enter a valid number.')
-    return option_product_categories
+            continue
+        else:
+            if option_product_categories == '7':
+                print('Going back to the previous page.')
+                product_management()
+            product_details()
+            continue_adding()
+        break
+
+
+'''if option_product_categories == '1':
+    category = 'Breads'
+elif option_product_categories == '2':
+    category = 'Cakes'
+elif option_product_categories == '3':
+    category = 'Pastries'
+elif option_product_categories == '4':
+    category = 'Biscuits'
+elif option_product_categories == '5':
+    category = 'Muffins'
+elif option_product_categories == '6':
+    while True:
+        category = input(f'\nCategory: ')
+        if validation_empty_entries(category):
+            if not validation_alphabet_only(category):
+                print('Please enter a valid category name. (Cannot contain any special characters.)\n.')
+            else:
+                break
+elif option_product_categories == '7':
+    print('Going back to the previous page.')
+    product_management()
+
+break
+return category'''
 
 
 def validation_empty_entries(info):
@@ -104,8 +126,15 @@ def validation_alphanum_only(info):
         return False
 
 
+def validation_alphanum_space_only(info):
+    if re.fullmatch(r'[A-Za-z0-9 ]+', info):
+        return True
+    else:
+        return False
+
+
 def validation_alphabet_only(info):
-    if re.fullmatch(r'[A-Za-z ]+',info):
+    if re.search(r'[A-Za-z ]+', info):
         return True
     else:
         return False
@@ -135,6 +164,7 @@ def validation_date(info, date_format='%d-%m-%Y'):
 
 
 def product_details():
+    product_data = load_data_from_inventory_product()
 
     product_info = ['Product Name', 'Product Code', 'Batch Number', 'Date of Production', 'Shelf Life',
                     'Expiry Date', 'Recipe', 'Quantity Produced', 'Baker\'s Name', 'Allergens']
@@ -147,18 +177,19 @@ def product_details():
     print('\nPlease fill out the following fields to add a new product to the inventory:\n')
 
     while True:
+        quantity_produced = input(f'1. {product_info[7].ljust(max_length + 2)}: ')
+        if validation_empty_entries(quantity_produced):
+            if not validation_digit_only(quantity_produced):
+                print('Please enter a valid quantity produced. (Cannot contain any special characters.)\n.')
+            else:
+                quantity = int(quantity_produced)
+                break
+
+    while True:
         product_name = input(f'1. {product_info[0].ljust(max_length + 2)}: ')
         if validation_empty_entries(product_name):
             if not validation_alphabet_only(product_name):
                 print('Please enter a valid product name. (Cannot contain any digits and special characters.)\n.')
-            else:
-                break
-
-    while True:
-        product_code = input(f'2. {product_info[1].ljust(max_length + 2)}: ')
-        if validation_empty_entries(product_code):
-            if not validation_alphanum_only(product_code):
-                print('Please enter a valid product code. (Cannot contain any special characters.)\n.')
             else:
                 break
 
@@ -181,7 +212,7 @@ def product_details():
     while True:
         shelf_life = input(f'5. {product_info[4].ljust(max_length + 2)}: ')
         if validation_empty_entries(shelf_life):
-            if not validation_alphanum_only(shelf_life):
+            if not validation_alphanum_space_only(shelf_life):
                 print('Please enter a valid shelf life. (Cannot contain any special characters.)\n.')
             else:
                 break
@@ -203,14 +234,6 @@ def product_details():
                 break
 
     while True:
-        quantity_produced = input(f'8. {product_info[7].ljust(max_length + 2)}: ')
-        if validation_empty_entries(quantity_produced):
-            if not validation_alphanum_only(quantity_produced):
-                print('Please enter a valid quantity produced. (Cannot contain any special characters.)\n.')
-            else:
-                break
-
-    while True:
         baker_name = input(f'9. {product_info[8].ljust(max_length + 2)}: ')
         if validation_empty_entries(baker_name):
             if not validation_alphanum_only(baker_name):
@@ -226,18 +249,31 @@ def product_details():
             else:
                 break
 
-    product_data[product_code] = {
+    product_codes = []
+
+    for i in range(quantity):
+        while True:
+            product_code = input(f'2. Enter product code for item {i + 1}: ')
+            if validation_empty_entries(product_code):
+                if not validation_alphanum_only(product_code):
+                    print('Please enter a valid product code. (Cannot contain any special characters.)\n.')
+                else:
+                    product_codes.append(product_code)
+                break
+
+    product_data[batch_number] = {
         'product_name': product_name,
-        'product_code': product_code,
+        'product_code': product_codes,
+        'quantity_produced': quantity_produced,
         'batch_number': batch_number,
         'date_of_production': date_of_production,
         'shelf_life': shelf_life,
         'expiry_date': expiry_date,
         'recipe': recipe,
-        'quantity_produced': quantity_produced,
         'baker_name': baker_name,
         'allergens': allergens
     }
+
     save_info(product_data)
 
 
@@ -267,4 +303,3 @@ def update_product():
     print('------------------------------------------------')
     for index, key in enumerate(product, start=1):
         print(f'{index}. {key}')
-
