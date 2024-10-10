be1297ebc4f810f7f0d4a6855e776059b0ff7c44
Ng Yvonne <88yvonne.ng@gmail.com>
2024-10-10 19:47:10 +0800

    -

--- manager_financial_management.py
+++ manager_financial_management.py
@@ -28,6 +28,17 @@ def printed_centered(info):
     print('-' * 95)
 
 
+def validate_year(year):
+    if year.isdigit():
+        if len(year) == 4:
+            return True
+        else:
+            print('\n+------------------------------+')
+            print('|⚠️ Please enter a valid year. |')
+            print('+------------------------------+')
+            return False
+
+
 transaction_keeping = load_data_from_cashier_transaction_keeping()
 equipment_report_keeping = load_data_from_equipment_report_keeping()
 
@@ -51,13 +62,13 @@ def financial_management():
 
     for receipt_details in transaction_keeping.values():
         order_date = datetime.strptime(receipt_details['order_date'], '%d/%m/%Y')  # retrieve all date from order_list
-        total_income_so_far += receipt_details['total_spend']
+        total_income_so_far += receipt_details['total_spend(RM)']
 
         if order_date == datetime.now().strftime('%d/%m/%Y'):
-            today_income += receipt_details['total_spend']
+            today_income += receipt_details['total_spend(RM)']
 
         if order_date.month == datetime.now().month:
-            this_month_income += receipt_details['total_spend']
+            this_month_income += receipt_details['total_spend(RM)']
 
     while True:
         print(f'\n💰 Today\'s {"Income":<9}: RM {today_income:.2f}{"":<25}'
@@ -70,65 +81,85 @@ def financial_management():
         if track_choice == '1':
             while True:
                 try:
-                    custom_year_for_income = int(input('\nInsert a year (or enter "0" to cancel):\n>>> '))
-                    if custom_year_for_income == 0:
+                    custom_year_for_income = input('\nInsert a year (or enter "0" to cancel):\n>>> ')
+                    if int(custom_year_for_income) == 0:
                         break
-                    custom_month_for_income = input('\nInsert a month (or enter "cancel"):\n>>> ').title()
-                    if custom_month_for_income.lower() == 'cancel':
-                        continue
-                    while custom_month_for_income not in months:
-                        print('\n+-------------------------------------+')
-                        print('|⚠️ Invalid month. Please enter again. |')
-                        print('+--------------------------------------+')
-                        custom_month_for_income = input('\nInsert a month (or enter "cancel"):\n>>> ').title()
+
+                    elif validate_year(custom_year_for_income):
+                        custom_month_for_income = input('\nInsert a month (or enter "cancel" to reselect year):\n>>> ').title()
                         if custom_month_for_income.lower() == 'cancel':
-                            break
+                            continue
+                        while custom_month_for_income not in months:
+                            print('\n+-------------------------------------+')
+                            print('|⚠️ Invalid month. Please enter again. |')
+                            print('+--------------------------------------+')
+                            custom_month_for_income = input('\nInsert a month (or enter "cancel" to reselect year):\n>>> ').title()
+                            if custom_month_for_income.lower() == 'cancel':
+                                break
 
-                    if custom_month_for_income in months:
-                        income_of_that_month = 0
-                        for receipt_details in transaction_keeping.values():
-                            chosen_date = datetime.strptime(receipt_details['order_date'], '%d/%m/%Y')
-                            if chosen_date.strftime("%B") == custom_month_for_income and chosen_date.year == custom_year_for_income:
-                                income_of_that_month += receipt_details['total_spend(RM)']
-                        print(f'\n💰 {custom_month_for_income.title()} Income: RM {income_of_that_month:.2f}')
-
+                        if custom_month_for_income in months:
+                            income_of_that_month = 0
+                            for receipt_details in transaction_keeping.values():
+                                chosen_date = datetime.strptime(receipt_details['order_date'], '%d/%m/%Y')
+                                if chosen_date.strftime("%B") == custom_month_for_income and chosen_date.year == custom_year_for_income:
+                                    income_of_that_month += receipt_details['total_spend(RM)']
+                            print(f'\n💰 {custom_year_for_income} {custom_month_for_income.title()} Income: RM {income_of_that_month:.2f}')
                 except ValueError:
                     print('\n+-------------------------------------+')
                     print('|⚠️ Invalid year. Please enter again. |')
-                    print('+--------------------------------------+')
+                    print('+-------------------------------------+')
 
         elif track_choice == '2':
             while True:
                 try:
-                    custom_year_for_expenses = int(input('\nInsert a year (or enter "0" to cancel):\n>>> '))
-                    if custom_year_for_expenses == 0:
+                    custom_year_for_expenses = input('\nInsert a year (or enter "0" to cancel):\n>>> ')
+                    if int(custom_year_for_expenses) == 0:
                         break
-                    custom_month_for_expenses = input('\nInsert a month (or enter "cancel"):\n>>> ').title()
-                    if custom_month_for_expenses.lower() == 'cancel':
-                        continue
-                    while custom_month_for_expenses not in months:
-                        print('\n+-------------------------------------+')
-                        print('|⚠️ Invalid month. Please enter again. |')
-                        print('+--------------------------------------+')
-                        custom_month_for_expenses = input('\nInsert a month (or enter "cancel"):\n>>> ').title()
-                        if custom_month_for_expenses.lower() == 'cancel':
-                            break
 
-                    if custom_month_for_expenses in months:
-                        expenses_of_that_month = 0
-                        for equipment_details in equipment_report_keeping.values():
-                            unit, repair_cost = equipment_details['repair_cost'].split(' ')
-                            chosen_date = datetime.strptime(equipment_details['report_date'], '%d-%m-%Y')
-                            if chosen_date.strftime("%B") == custom_month_for_expenses and chosen_date.year == custom_year_for_expenses:
-                                expenses_of_that_month += float(repair_cost)
-                        print(f'\n💰 {custom_month_for_expenses.title()} Expenses: RM {expenses_of_that_month:.2f}')
+                    elif validate_year(custom_year_for_expenses):
+                        custom_month_for_expenses = input('\nInsert a month (or enter "cancel" to reseelct year):\n>>> ').title()
+                        if custom_month_for_expenses.lower() == 'cancel':
+                            continue
+                        while custom_month_for_expenses not in months:
+                            print('\n+-------------------------------------+')
+                            print('|⚠️ Invalid month. Please enter again. |')
+                            print('+--------------------------------------+')
+                            custom_month_for_expenses = input('\nInsert a month (or enter "cancel" to reselect year):\n>>> ').title()
+                            if custom_month_for_expenses.lower() == 'cancel':
+                                break
+
+                        if custom_month_for_expenses in months:
+                            expenses_of_that_month = 0
+                            for equipment_details in equipment_report_keeping.values():
+                                unit, repair_cost = equipment_details['repair_cost'].split(' ')
+                                chosen_date = datetime.strptime(equipment_details['report_date'], '%d-%m-%Y')
+                                if chosen_date.strftime("%B") == custom_month_for_expenses and chosen_date.year == custom_year_for_expenses:
+                                    expenses_of_that_month += float(repair_cost)
+                            print(f'\n💰 {custom_year_for_expenses} {custom_month_for_expenses.title()} Expenses: RM {expenses_of_that_month:.2f}')
 
                 except ValueError:
                     print('\n+-------------------------------------+')
                     print('|⚠️ Invalid year. Please enter again. |')
-                    print('+--------------------------------------+')
+                    print('+-------------------------------------+')
+
+        elif track_choice == '3':
+            pass
+
+        elif track_choice == '4':
+            while True:
+                try:
+                    custom_year_for_expenses = input('\nInsert a year (or enter "0" to cancel):\n>>> ')
+                    if int(custom_year_for_expenses) == 0:
+                        break
+
+                    elif validate_year(custom_year_for_expenses):
+                        print('')
 
 
+                except ValueError:
+                    print('\n+-------------------------------------+')
+                    print('|⚠️ Invalid year. Please enter again. |')
+                    print('+-------------------------------------+')
 
 
 financial_management()
